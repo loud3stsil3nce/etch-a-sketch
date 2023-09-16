011f9db306b683a840427f8d8e9436b05dc87bd1
Rafiur Rahman <rafiur@Xubuntu.myguest.virtualbox.org>
2023-09-16 00:18:04 -0400

    Added new mouseover event, instead of css hover. Now, when you hover over a box, it will permanently change color because of the class added to the selected div. In addition, I created a new button, the Clear button. When clicked, it will return the canvass to the original state.

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const gridContainer = document.querySelector('#divContainer');
 
 
+
 function makeCells() {
     for (let i = 0; i < 16;i++ ) {
         const gridRow = document.createElement("div");
         gridRow.classList.add('row');
         gridContainer.appendChild(gridRow);
-        //gridRow.textContent = "testt"
         for (let i = 0; i < 16; i++) {
             const gridColumn = document.createElement("div");
             gridColumn.classList.add('column');
             gridRow.appendChild(gridColumn);
-            gridColumn.textContent = " ";
-            }
+                gridColumn.addEventListener('mouseover', 
+                e => e.target.classList.add('cola')) 
+                const clear = document.querySelector('.clear');
+                clear.addEventListener('click', clearPage)
+                
+                function clearPage() {
+                gridColumn.classList.remove('cola')
+                }   
+        }   
     }
 }
 
-
 makeCells();
+
+
+
